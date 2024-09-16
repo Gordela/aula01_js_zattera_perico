89f08e3aa349140b450dba6e602bbf44b25397dd
Gordela <gabrielzattera86@gmail.com>
2024-09-16 08:39:08 -0300

    Add files via upload

--- aula01_JS_zattera_perico/1-meuPrimeiroPrograma.js
+++ aula01_JS_zattera_perico/1-meuPrimeiroPrograma.js
@@ -1,15 +1,18 @@
 //primeira instrução em JS
-console.log("\nOlá mundo cruel da programação\n");
+console.log("Olá mundo cruel da programação\n");
 
 const aluno1 = "Zattera";
-const aluno2 = "Perico";
+const aluno2 = "Périco";
 
 const idadeAluno1 = 36;
 const idadeAluno2 = 43;
 
-const somaIdades = idadeAluno1+idadeAluno2;
+const somaIdades = idadeAluno1 + idadeAluno2;
 
-const dupla = aluno1+ " e " +aluno2;
+const dupla = aluno1 + " e " + aluno2;
 
-console.log("Somos a dupla: "+dupla+"\n");
-console.log("A somo de nossas idades é: "+somaIdades+" anos");
+console.log("Somos a dupla: " + dupla + "\n");
+console.log("A soma de nossas idades é: " + somaIdades + " anos");
+
+document.write("<h1>Somos a dupla: </h1>" + dupla + "\n");
+document.write("A soma de nossas idades é: " + somaIdades + " anos");
